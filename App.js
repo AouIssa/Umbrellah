35d8082c4363cdd38613c4984b47d61c10810c6b
AouIssa <spunkyguyyt@gmail.com>
2022-11-22 13:46:23 +0800

    Added Splash screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react';  
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>heyyy</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "space-between",
+    backgroundColor: "#000",
+    alignItems: "center",
+    width: "100%",
   },
+
 });
