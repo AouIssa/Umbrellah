4a4c9be0cf5bd2eb9d41724716e331069c4c1a3f
AouIssa <spunkyguyyt@gmail.com>
2022-11-24 11:20:19 +0800

    Email field

--- components/style.js
+++ components/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import { View, Image, Text} from 'react-native';
-import  Constants  from 'expo-constants';
+import { View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
 
 const StatusBarHeight = Constants.statusBarHeight;
 
@@ -40,9 +40,8 @@ export const PageTitle = styled.Text`
   text-align: center;
   font-weight: bold;
   color: ${brand};
-  padding: 10px
+  padding: 10px;
 `;
-
 
 export const SubTitle = styled.Text`
   font-size: 18px;
@@ -50,4 +49,56 @@ export const SubTitle = styled.Text`
   letter-spacing: 1px;
   font-weight: bold;
   color: ${tertiary};
+`;
+
+export const StyledFormArea = styled.View`
+  width: 90%;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  background-color: ${secondary};
+  padding: 15px;
+  padding-left: 55px;
+  padding-right: 55px;
+  border-radius: 5px;
+  font-size: 16px;
+  height: 60px;
+  margin-vertical: 3px;
+  margin-botom: 10px;
+  color: ${tertiary};
+`;
+
+export const StyledInputLabel = styled.Text`
+  color: ${tertiary};
+  font-size: 13px;
+  text-align: left;
+`;
+
+export const LeftIcon = styled.View`
+  left: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+  left: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${brand};
+  justify-content: center;
+  border-radius: 5px;
+  maring-vertical: 5px;
+  height: 60px;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${primary};
+  font-size: 16px;
+  
 `;
--- screens/Login.js
+++ screens/Login.js
@@ -1,19 +1,75 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
-import { StyledContainer, InnerContainer, PageLogo, PageTitle } from './../components/style';
+// formik
+import { Formik } from 'formik';
+
+//icons
+import { Octicons } from '@expo/vector-icons';
+
+import {
+  StyledContainer,
+  InnerContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  LeftIcon,
+  StyledInputLabel,
+  StyledTextInput,
+  RightIcon,
+  Colors,
+} from './../components/style';
+import { View } from 'react-native';
+
+//colors
+const { brand, darkLight } = Colors;
 
 const Login = () => {
   return (
     <StyledContainer>
       <StatusBar style="dark" />
-
       <InnerContainer>
         <PageLogo resizeMode="cover" source={require('./../assets/Logo.png')} />
         <PageTitle>Umbrellah</PageTitle>
+        <SubTitle>Account Login</SubTitle>
+
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values) => {
+            console.log('Login');
+          }}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) =>( 
+          <StyledFormArea>
+            <MyTextinput
+            label="Email Address"
+            icon = "mail"
+            placeholder = "yourEmail@example.com"
+            placeholderTextColor = {darkLight}
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+            value={values.email}
+            keyboardType= "email-address"
+            
+            />
+
+          </StyledFormArea>)}
+        </Formik>
       </InnerContainer>
     </StyledContainer>
   );
 };
 
+const MyTextinput = ({ label, icon, ...props }) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel> {label} </StyledInputLabel>
+      <StyledTextInput {...props} />
+    </View>
+  );
+};
 export default Login;
